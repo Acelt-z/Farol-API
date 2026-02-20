a2a03447d7c8089d2a1c83846e8efe7f136cf5f6
Allan Matias <alangiovanepaes@gmail.com>
2026-02-20 14:39:11 -0300

    core: updating index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,35 @@ import http from "http";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import { config } from "dotenv";
+import cors from 'cors';
+import cookie from 'cookie-parser';
 
 import { swaggerSpec } from "./utils/swagger.js";
 import logger from "./utils/logger.js";
+import PublicRoutes from './routes/public.js';
 
-// Inicializa o DotEnv
 config();
 
-const PORT = process.env.PORT ?? 3000; // Porta do serviço
+const PORT = process.env.PORT ?? 3000;
 
-// Servidor e aplicação
+// Server
 const app = express();
 const server = http.createServer(app);
 
-// Rota de testes
-app.get("/ping", (_req, res) => {
-  res.status(200).json({ message: "Pong!" });
-});
+// Cors & Cookies
+app.use(cors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
+    credentials: true,
+}));
+app.use(cookie());
 
-// Rota de documentação
+
+// Documentation
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// Inicializa o servidor
+app.use('/', PublicRoutes)
+
+// Initialize server
 server.listen(PORT, () => {
   logger.info(`Server running on PORT: ${PORT}`);
 });
