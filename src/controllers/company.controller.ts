a3e4e4ae8bf8dd962a4494598495639b0c95eae4
Allan Matias <alangiovanepaes@gmail.com>
2026-02-26 16:08:13 -0300

    refactor: documentating + changing response

--- src/controllers/company.controller.ts
+++ src/controllers/company.controller.ts
@@ -1,21 +1,13 @@
 import {type Request, type Response} from 'express';
-import { getParsedData } from '../utils/utils.js';
+import { assertUserIdxists, getParsedData } from '../utils/utils.js';
 import { CreateCompanySchema } from '../models/company.js';
-import { ErrorCodes } from '../errors/interfaces/errorCodes.js';
-import { AppError } from '../errors/AppError.js';
 import { CompanyService } from '../services/CompanyService.js';
 import { prisma } from '../utils/prisma.js';
 
 const companyService = new CompanyService(prisma);
 
 export async function createCompanyController(req: Request, res: Response) {
-    if (!req.userId) {
-        throw new AppError({
-            message: "Unauthorized",
-            errorCode: ErrorCodes.UNAUTHORIZED,
-            statusCode: 401
-        });
-    }
+    assertUserIdxists(req.userId);
 
 
     const result = CreateCompanySchema.safeParse(req.body);
@@ -30,18 +22,23 @@ export async function createCompanyController(req: Request, res: Response) {
 }
 
 export async function getUserCompaniesController(req: Request, res: Response) {
-     if (!req.userId) {
-        throw new AppError({
-            message: "Unauthorized",
-            errorCode: ErrorCodes.UNAUTHORIZED,
-            statusCode: 401
-        });
-    }
+    assertUserIdxists(req.userId);
 
     
     const companies = await companyService.getUserCompanies(req.userId);
     
     return res.status(201).json({
+        success: true,
+        data: companies
+    });
+}
+
+export async function getUserCompaniesCards(req: Request, res: Response) {
+    assertUserIdxists(req.userId);
+    
+    const companies = await companyService.getUserCompaniesCard(req.userId);
+    
+    return res.status(200).json({
         success: true,
         data: companies
     });
--- src/models/company.ts
+++ src/models/company.ts
@@ -1,5 +1,5 @@
 import { isCnpj } from "validator-brazil";
-import type { Company } from "../generated/prisma/client.js";
+import type { Company, CompanyStatus } from "../generated/prisma/client.js";
 import {z } from "zod";
 
 
@@ -14,8 +14,24 @@ export type CompanyResponseDTO = {
     number: number;
     complement?: string | null;
     trialEndsAt?: Date | null;
+    status: CompanyStatus;
     planId: number;
     ownerId?: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+export type CompanyCardResponseDTO = {
+    id: string;
+    name: string;
+    cnpj: string;
+    city: string;
+    uf: string;
+    totalWorkers: number;
+    trialEndsAt?: Date | null;
+    status: CompanyStatus;
+    ownerId: string;
+    planId: number;
     createdAt: Date;
     updatedAt: Date;
 }
@@ -33,10 +49,10 @@ export const CreateCompanySchema = z.object({
 
 export type CreateCompanyDTO = z.infer<typeof CreateCompanySchema>;
 
-type ResponseArgs = {company: Company, totalWorkers: number, userRole?: "OWNER" | "MEMBER" | null};
+type ResponseArgs = {company: Company, totalWorkers: number};
 
 export class CompanyMapper {
-    static toResponse({company, totalWorkers}: ResponseArgs): CompanyResponseDTO{
+    static toCompleteResponse({company, totalWorkers}: ResponseArgs): CompanyResponseDTO{
         return {
             id: company.id,
             name: company.name,
@@ -48,6 +64,24 @@ export class CompanyMapper {
             complement: company.complement,
             totalWorkers,
             trialEndsAt: company.trialEndsAt || null,
+            status: company.status,
+            ownerId: company.ownerId,
+            planId: company.planId,
+            createdAt: company.createdAt,
+            updatedAt: company.updatedAt
+        };
+    }
+
+    static toCardResponse({company, totalWorkers}: ResponseArgs): CompanyCardResponseDTO{
+        return {
+            id: company.id,
+            name: company.name,
+            cnpj: company.cnpj,
+            city: company.city,
+            uf: company.uf,
+            totalWorkers,
+            trialEndsAt: company.trialEndsAt || null,
+            status: company.status,
             ownerId: company.ownerId,
             planId: company.planId,
             createdAt: company.createdAt,
