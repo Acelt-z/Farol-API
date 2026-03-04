a335c2ee43d7f496dee490a2b23492e0e69e574b
Allan Matias <alangiovanepaes@gmail.com>
2026-03-03 22:29:33 -0300

    feat: mapping Todos

--- src/routes/private/companyRoutes.ts
+++ src/routes/private/companyRoutes.ts
@@ -12,10 +12,28 @@ router.patch('/:companyId', async (req, res) => {
     await updateCompanyController(req, res);
 });
 
+router.patch('/:companyId/plan', async (_req, _res) => {
+    // TODO: Make plan update
+});
 
+router.patch('/:companyId/status', async (_req, _res) => {
+    // TODO: Suspend/cancel/reactivate
+});
+
+router.patch('/:companyId/transfer-ownership', async (_req, _res) => {
+    // TODO: Make transfer ownership
+});
+
+router.delete('/:companyId', async (_req, _res) => {
+    // TODO: Make company deletion
+});
+
+
+// -------- Branches ---------
 
 router.post('/:parentCompanyId/branch', async (req, res) => {
     await createBranchCompanyController(req, res);
 });
+
 
 export default router;
